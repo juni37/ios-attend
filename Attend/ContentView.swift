233697de64f7c875bf7def18c2002ef450509704
sjydavid <sy105@duke.edu>
2020-04-26 13:48:43 +0900

    Refactored ContentView.swift

--- Attend/ContentView.swift
+++ Attend/ContentView.swift
@@ -12,25 +12,31 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List{
-                HStack {
-                    VStack (alignment: .center, spacing: 10) {
-                        Image(systemName: "calendar.circle.fill")
-                            .resizable()
-                            .frame(width: 30, height: 30, alignment: .center)
-                            .foregroundColor(.green)
-
-                        Text("Today")
-                            .foregroundColor(Color.black.opacity(0.5))
-                        .bold()
-                    }.padding()
-                    Spacer()
-                    Text("6")
-                        .bold()
-                        .font(.system(size: 30))
-                    .padding()
-                }
+                ReminderView()
             }
             .navigationBarTitle("수업")
+        }
+    }
+}
+
+struct ReminderView: View {
+    var body: some View {
+        HStack {
+            VStack (alignment: .center, spacing: 10) {
+                Image(systemName: "calendar.circle.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30, alignment: .center)
+                    .foregroundColor(.green)
+
+                Text("Today")
+                    .foregroundColor(Color.black.opacity(0.5))
+                .bold()
+            }.padding()
+            Spacer()
+            Text("6")
+                .bold()
+                .font(.system(size: 30))
+            .padding()
         }
     }
 }
