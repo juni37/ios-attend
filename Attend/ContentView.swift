03d1872a32f534a285131c21716afc619992064a
sjydavid <sy105@duke.edu>
2020-04-26 13:46:34 +0900

    Added Additional UIView -

--- Attend/ContentView.swift
+++ Attend/ContentView.swift
@@ -12,7 +12,23 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List{
-                Text("Hello World")
+                HStack {
+                    VStack (alignment: .center, spacing: 10) {
+                        Image(systemName: "calendar.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30, alignment: .center)
+                            .foregroundColor(.green)
+
+                        Text("Today")
+                            .foregroundColor(Color.black.opacity(0.5))
+                        .bold()
+                    }.padding()
+                    Spacer()
+                    Text("6")
+                        .bold()
+                        .font(.system(size: 30))
+                    .padding()
+                }
             }
             .navigationBarTitle("수업")
         }
